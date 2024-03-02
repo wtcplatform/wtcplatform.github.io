97d706403d4c36469a7aa66b707c06b985024fe2
wtcplatform <wtcplatform7@gmail.com>
2024-03-02 18:22:14 +0900

    modified layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,12 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/SjnLID15jwa
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
+import Link from "next/link"
+import React from "react"
+import { Package2Icon } from "@/components/ui/icons"
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,7 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div key="1" className="flex flex-col w-full min-h-screen">
+        <header className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
+          <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="#">
+            <Package2Icon className="w-6 h-6" />
+            <span className="sr-only">Acme Inc</span>
+            wtc所沢コート予約システム
+          </Link>
+        </header>
+        { children }
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,98 +1,27 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, setDoc, doc, query, limit } from 'firebase/firestore/lite';
-import * as XLSX from 'xlsx';
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { firebaseConfig } from "@/lib/firebase";
-import { saveSortedDataToExcel } from "@/lib/xlsx";
-import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
+import AccountsComponent from "@/app/accounts";
+import ReservationComponent from "@/app/reservation";
 
-import type { User } from "@/lib/types";
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default function Home() {
-  const [users, setUsers] = useState<User[]>([]);
-  
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const userCollection = collection(db, 'accounts');
-      const firstFiveUsersQuery = query(userCollection, limit(5));
-      const userSnapshot = await getDocs(firstFiveUsersQuery);
-      
-      const userList = userSnapshot.docs.map(doc => ({
-        id: doc.id, // Include the document ID
-        ...doc.data()
-      })) as User[];
-      console.log(userList);
-      setUsers(userList);
-    };
-
-    fetchUsers();
-  }, []);
-
-  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-      const data = e.target?.result;
-      const workbook = XLSX.read(data, { type: 'binary' });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      const json: User[] = XLSX.utils.sheet_to_json(worksheet, { raw: true });
-
-      for (const user of json) {
-        if (user.id) {
-          try {
-            await setDoc(doc(db, "accounts", user.id), user);
-            console.log('User added successfully:', user.id);
-          } catch (error) {
-            console.error("Error adding user:", user.id, error);
-          }
-        } else {
-          console.warn('User skipped (Missing ID):', user);
-        }
-      }
-      alert('Data upload completed!');
-    };
-    reader.readAsBinaryString(file);
-  };
-
-  const handleClick = async () => {
-    const userCollection = collection(db, 'accounts');
-    const userSnapshot = await getDocs(userCollection);
-
-    const userList = userSnapshot.docs.map(doc => ({
-      ...doc.data()
-    })) as User[];
-    saveSortedDataToExcel(userList, 'sortedData.xlsx');
-  };
-
-  return (
-    <Card className="w-full max-w-lg">
-      <CardHeader>
-        <CardTitle className="text-xl">Manage Users</CardTitle>
-        <CardDescription>1. 既存のユーザー一覧をダウンロード 2.新規会員を追加したxlsxファイルをアップロード.xlsx</CardDescription>
-      </CardHeader>
-      <CardContent className="flex gap-4 items-start py-4">
-        <div className="flex flex-col items-center space-y-4">
-          <Button className="px-4 py-2" onClick={handleClick}>ユーザー一覧をダウンロード</Button>
-          <p className="text-sm text-gray-500">Update Users (Please add rows to the downloaded user list.)</p>
-          <input
-            accept=".xlsx"
-            className="w-full max-w-sm border border-gray-300 rounded-md py-2 px-4 text-sm"
-            id="upload"
-            type="file"
-            onChange={handleUpload}
-          />
+export default function Page() {
+    return (
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 items-start">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">アカウント一覧</h1>
+          <div className="flex border rounded-lg border-dashed border-gray-200 dark:border-gray-800">
+            <AccountsComponent/>
+          </div>
         </div>
-      </CardContent>
-    </Card>
-  );
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">予約</h1>
+          <div className="flex border rounded-lg border-dashed border-gray-200 dark:border-gray-800">
+            <ReservationComponent/>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">予約の確認</h1>
+          <div className="flex border rounded-lg h-[200px] border-dashed border-gray-200 dark:border-gray-800">
+          {/* {children && children.reservationConfirmation ? children.reservationConfirmation : "Cannot find children.reservationConfirmation"} */}
+          </div>
+        </div>
+      </main>
+    );
 }
